3cc0ff6eef87c581fd41779b05de19e8847dfffd
Prasanna Venkatesh <prasannavenkatesh.ramkumar@iiitb.org>
2020-06-18 19:17:50 +0530

    Add files via upload

---
+++ UdemyCourse - 40 projects/Milesconverter.py
@@ -0,0 +1,5 @@
+print("Welcome to the MPH to MPS Conversion App")
+
+mph = float(input('What is your speed in Miles per Hour : '))
+mps = round(mph * 0.4474,2)
+print("Your speed in meters per second is {}".format(mps))
